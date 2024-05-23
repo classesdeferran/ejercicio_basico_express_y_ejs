bf66fc694a75ba3c7a3978fb0c0407b21bc21fd3
Ferran <ferran.docencia@gmail.com>
2024-05-23 08:38:02 +0200

    Ejercicio sencillo con express y ejs

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+
+const port = 3000;
+
+app.set("view engine", "ejs");
+// app.set('views', __dirname + '/views');
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+  //   res.sendFile('index.html', {root: __dirname + '/public'});
+  res.render("index", {
+    titulo: "index",
+    parrafo: "soy un texto EJS",
+  });
+});
+app.get("/alumnos", (req, res) => {
+  //   res.sendFile('index.html', {root: __dirname + '/public'});
+  res.render("alumnos", {
+    titulo: "alumnos",
+    parrafo: "soy un texto EJS",
+    footer: "Soy el footer de alumnos"
+  });
+});
+app.get("/cursos", (req, res) => {
+  //   res.sendFile('index.html', {root: __dirname + '/public'});
+  res.render("cursos", {
+    titulo: "cursos",
+    parrafo: "soy un texto EJS",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Servidor en http://localhost:${port}`);
+});
